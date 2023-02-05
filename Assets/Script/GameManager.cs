1bdac9976a3c0a28652fcacbfdf6474eef51ea9f
奥山 <83827729+Nana-77-o@users.noreply.github.com>
2023-02-06 00:39:55 +0900

    [Add]新しいアイテム、結果演出の追加 [Update]操作性、起こりそうなバグ潰し

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -21,18 +21,30 @@ public class GameManager : MonoBehaviour
     private Text _countDownText = null;
     [SerializeField, Tooltip("ゲームスタート時にすること")]
     private UnityEvent _gameStart;
+    [SerializeField, Header("ゲーム終了時")]
+    private GameObject _clerePanel = null;
+    [SerializeField]
+    private Text _sore = null;
+    [SerializeField]
+    private Text _text = null;
+    [SerializeField]
+    private GameObject _moneys;
+    [SerializeField, Tooltip("ゲーム終了時にすること")]
+    private UnityEvent _gameFinisht;
 
     private GameObject _newPlayer = null;
     private bool _startGame = false;
+    private bool _gameClere = false;
     private int _playerId = 0;
+    private string[] _textList = null;
+    private const float NEXT_TEXT = 1f;
 
     void Start()
     {
-        if(CharacterCelect.Instance == null) { _playerId = 0; }
-        else {_playerId = CharacterCelect.Instance.PlayerIndex; }
+        _playerId = CharacterCelect.Instance.PlayerIndex;
         _newPlayer = Instantiate(_players[_playerId]);
         _newPlayer.GetComponent<PlayerController>().SetUp(_playerData._playerDataBace[_playerId]);
-        _newPlayer.GetComponent<PlayerController>().enabled = false;
+        _newPlayer.GetComponent<PlayerController>().ScoreText = _scoreText;
     }
 
     void Update()
@@ -42,7 +54,7 @@ public class GameManager : MonoBehaviour
             _countDownTime -= Time.deltaTime;
             _countDownText.text = $"{_countDownTime:0}";
         }
-        else if(_countDownTime < 0)
+        else if(_countDownTime < 0 && !_startGame)
         {
             _countDownText.gameObject.SetActive(false);
             _startGame = true;
@@ -51,9 +63,42 @@ public class GameManager : MonoBehaviour
         }
         
         if (!_startGame) { return; }
+        if (_timer <= 0 && !_gameClere) { GameClear(); }
         _timer -= Time.deltaTime;
         int minutes = (int)_timer / 60;
         float seconds = _timer - minutes * 60;
         _timerText.text = $"{minutes:00}:{(int)seconds:00}";
+    }
+
+    /// <summary>
+    /// ゲーム終了時
+    /// </summary>
+    public void GameClear()
+    {
+        _gameClere = true;
+        _gameFinisht.Invoke();
+        _textList = new string[_scoreText.text.Length];
+        for (int i = 0; i < _scoreText.text.Length; i++)
+        {
+            var inputText = _scoreText.text.Substring(i, 1);
+            _textList[i] = inputText;
+        }
+        _sore.text = null;
+        StartCoroutine(UI());
+    }
+
+    /// <summary>
+    /// スコア発表演出
+    /// </summary>
+    IEnumerator UI()
+    {
+        _sore.gameObject.SetActive(true);
+        _text.gameObject.SetActive(true);
+        for(int i = _textList.Length -1; 0 <= i; i--)
+        {
+            _sore.text = $"{_textList[i]}{_sore.text}";
+            yield return new WaitForSeconds(NEXT_TEXT);
+        }
+        _moneys.SetActive(true);
     }
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,34 +8,45 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Tooltip("速度制限時間")]
     private int _decelerationTime = 3;
     [SerializeField, Tooltip("減速量")]
-    private int _deceleration = 2;
+    private float _deceleration = 2;
 
     private float _speed = 0f;
     private int _addScore;
     private int _totalScore = 0;
     private Text _scoreText = null;
+    [Tooltip("スピード調整用")]
+    private const float ADJUSTMENT = 0.025f;
 
     public Text ScoreText { get => _scoreText; set => _scoreText = value; }
 
     void Start()
     {
-        Debug.Log($"{_speed}");
+        _deceleration *= ADJUSTMENT;
     }
 
     private void FixedUpdate()
     {
         var h = Input.GetAxis("Horizontal");
-        var v = Input.GetAxis("Vertical");
+        if(h < 0) { gameObject.GetComponent<SpriteRenderer>().flipX = false; }
+        else if(h > 0) { gameObject.GetComponent<SpriteRenderer>().flipX = true; }
         transform.position += new Vector3(h * _speed, 0, 0);
     }
 
     public void SetUp(PlayerDataBace data)
     {
-        _speed = data.Speed;
+        _speed = data.Speed * ADJUSTMENT;
     }
 
+    /// <summary>
+    /// 減速してしまうアイテム取得時
+    /// </summary>
     IEnumerator Weight()
     {
+        if(_speed < _deceleration) 
+        { 
+            Debug.Log("減速量がもとのスピードより多い");
+            yield break; 
+        }
         _speed -= _deceleration;
         yield return new WaitForSeconds(_decelerationTime);
         _speed += _deceleration;
@@ -47,7 +58,7 @@ public class PlayerController : MonoBehaviour
         {
             _addScore = collision.gameObject.GetComponent<Item>().Score;
             _totalScore += _addScore;
-            ScoreText.text = $"{_totalScore}";
+            ScoreText.text = $"{_totalScore:000,000,000}";
         }
         if (collision.gameObject.CompareTag("Weight"))
         {
